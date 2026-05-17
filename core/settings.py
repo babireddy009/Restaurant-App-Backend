65c4b47b9a368cec4a5ac63dba689a3ae47494bb
babireddy009 <babireddy009@gmail.com>
2026-05-17 15:18:35 +0530

    Temporarily force DEBUG=True

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-restaurant-dev-key-change-in-prod')
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*,localhost,127.0.0.1,.onrender.com,msrrayalaseemaruchulu.com,www.msrrayalaseemaruchulu.com').split(',')
 
